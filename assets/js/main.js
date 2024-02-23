eb54012bc276130b39ab2f966f8c07899deb7e6d
simone-nolberto <simone.nolberto@outlook.com>
2024-02-23 17:37:24 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,47 @@ console.log(slides);
 for (let i = 0; i < imgList.length; i++) {
     const img = imgList[i];
     console.log(img);
-    
-    const slidesContent = `<img class=${ i === currentImg ? 'active': '' } src="./assets/img/${img}" alt="">`;
+
+    const slidesContent = `<img class="${i === currentImg ? 'active' : ''}" src="./assets/img/${img}" alt="">`;
 
     console.log(slidesContent);
 
     slides.insertAdjacentHTML('beforeend', slidesContent);
     console.log();
- 
+
 
 }
+
+const prevBtn = document.querySelector('.prev');
+
+const nextBtn = document.querySelector('.next');
+
+
+prevBtn.addEventListener('click', function () {
+    console.log("previous img");
+    currentImg--;
+    const activeImg = document.querySelector('img.active');
+    console.log(activeImg);
+    activeImg.classList.remove('active');
+
+    const allImg = document.querySelectorAll('.slides img')
+
+    allImg[currentImg].classList.add('active');
+
+})
+
+
+nextBtn.addEventListener('click', function () {
+    console.log("next img");
+    currentImg++;
+    const activeImg = document.querySelector('img.active');
+    console.log(activeImg);
+    activeImg.classList.remove('active');
+
+    const allImg = document.querySelectorAll('.slides img')
+
+    allImg[currentImg].classList.add('active');
+    console.log(allImg[currentImg]);
+
+
+})
